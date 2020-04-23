b78052a9325c7a6c90275a5aec5aac0d43fb279e
Алексей Гагарин <alex4619748@rambler.ru>
2020-04-23 17:26:10 +0300

    Добавьте файлы проекта.

---
+++ Mvc.Web/Controllers/SalesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Mvc.Web.Models;
+
+namespace Mvc.Web.Controllers
+{
+    public class SalesController : Controller
+    {
+        private AnseremDbEntities db = new AnseremDbEntities();
+
+        // GET: Sales
+        public ActionResult Index()
+        {
+            var sales = db.Sales.Include(s => s.Сities).Include(s => s.Сontacts).Include(s => s.Сounterpartys);
+            return View(sales.ToList());
+        }
+
+        // GET: Sales/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sales sales = db.Sales.Find(id);
+            if (sales == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sales);
+        }
+
+        // GET: Sales/Create
+        public ActionResult Create()
+        {
+            ViewBag.CityId = new SelectList(db.Сities, "CityId", "CityName");
+            ViewBag.ContactId = new SelectList(db.Сontacts, "ContactId", "CounterpartyContact");
+            ViewBag.СounterpartyId = new SelectList(db.Сounterpartys, "СounterpartyId", "СounterpartyName");
+            return View();
+        }
+
+        // POST: Sales/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SaleId,SaleName,СounterpartyId,ContactId,CityId")] Sales sales)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sales.Add(sales);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CityId = new SelectList(db.Сities, "CityId", "CityName", sales.CityId);
+            ViewBag.ContactId = new SelectList(db.Сontacts, "ContactId", "CounterpartyContact", sales.ContactId);
+            ViewBag.СounterpartyId = new SelectList(db.Сounterpartys, "СounterpartyId", "СounterpartyName", sales.СounterpartyId);
+            return View(sales);
+        }
+
+        // GET: Sales/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sales sales = db.Sales.Find(id);
+            if (sales == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CityId = new SelectList(db.Сities, "CityId", "CityName", sales.CityId);
+            ViewBag.ContactId = new SelectList(db.Сontacts, "ContactId", "CounterpartyContact", sales.ContactId);
+            ViewBag.СounterpartyId = new SelectList(db.Сounterpartys, "СounterpartyId", "СounterpartyName", sales.СounterpartyId);
+            return View(sales);
+        }
+
+        // POST: Sales/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SaleId,SaleName,СounterpartyId,ContactId,CityId")] Sales sales)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sales).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CityId = new SelectList(db.Сities, "CityId", "CityName", sales.CityId);
+            ViewBag.ContactId = new SelectList(db.Сontacts, "ContactId", "CounterpartyContact", sales.ContactId);
+            ViewBag.СounterpartyId = new SelectList(db.Сounterpartys, "СounterpartyId", "СounterpartyName", sales.СounterpartyId);
+            return View(sales);
+        }
+
+        // GET: Sales/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sales sales = db.Sales.Find(id);
+            if (sales == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sales);
+        }
+
+        // POST: Sales/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sales sales = db.Sales.Find(id);
+            db.Sales.Remove(sales);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Mvc.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется следующим образом
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("Mvc.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mvc.Web")]
+[assembly: AssemblyCopyright("© , 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM.  Если требуется обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для ID библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("c31341e7-b80b-42d7-b2a7-684b9e4e0ff9")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      Дополнительный номер версии
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер редакции и номер сборки по умолчанию,
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
